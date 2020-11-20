a18569980124a21a70fafb10d9c4a32cec5dd6a8
Nawrok <nawrok999@gmail.com>
2020-11-20 13:51:46 +0100

    Initial commit

---
+++ src/test/java/pl/nbd/imdb/ImdbApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.nbd.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
