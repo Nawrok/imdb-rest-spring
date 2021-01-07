9358c308177143184c803ccd48135414f11e2b60
Nawrok <nawrok999@gmail.com>
2021-01-07 12:48:01 +0100

    Improved docker-compose.yml

--- scripts/mongo-cluster-setup.sh
+++ scripts/mongo-cluster-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 10
-mongo --host mongo1 --eval "rs.initiate({_id : 'rs0','members' : [{_id : 0, host : 'mongo1:27017'},{_id : 1, host : 'mongo2:27017'},{_id : 2, host : 'mongo3:27017'}]})"
+sleep 5
+mongo --host mongo1 --eval "rs.initiate({_id : 'rs0','members' : [{_id : 0, host : 'mongo1:27017', priority : 2},{_id : 1, host : 'mongo2:27017'},{_id : 2, host : 'mongo3:27017'}]})"
